44f3fde34eb45497cf403877ea6c0ea87ba4600e
Daniel Kogeni <40920446+dan-k3@users.noreply.github.com>
2025-07-18 21:28:37 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
+vim.rocks.setup({
+  enabled   = false,
+  hererocks = false,
+})
+
 require("config.lazy")
 
--- Enable line number
+-- Core editing prefs
 vim.opt.number = true
 vim.opt.relativenumber = true
-
--- Misc.
 vim.opt.shiftwidth = 2
---vim.opt.rocks.hererocks = false
---vim.opt.rocks.enabled = false
