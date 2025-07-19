fe99ee3b7b9bf24397ae468b91a522c79baa53a4
Dan <40920446+dan-k3@users.noreply.github.com>
2025-07-18 21:21:22 -0500

    Initial commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+
+-- Enable line number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Misc.
+vim.opt.shiftwidth = 2
+--vim.opt.rocks.hererocks = false
+--vim.opt.rocks.enabled = false
