4a766b3e5dc83e33fe6751147a5ae487f56c6c63
Dan <40920446+dan-k3@users.noreply.github.com>
2025-10-24 22:59:26 -0500

    Update Neovim LSP configuration and dependencies

--- init.lua
+++ init.lua
@@ -1,3 +1,43 @@
 require("config.options")
 require("config.remap")
 require("config.lazy")
+
+-- Netrw settings
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4 -- Open in previous window (default is 0)
+
+vim.opt.splitright = true
+
+-- Configure LSP clients
+
+-- Set default root markers for all clients
+vim.lsp.config('*', {
+  root_markers = { '.git' },
+})
+
+-- Set configuration for typescript language server
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+})
+
+-- Enable Typescript Language Server
+vim.lsp.enable('ts_ls')
+
+-- Set configuration for python language server
+vim.lsp.config('pyright', {
+  cmd = { 'pyright-langserver', '--stdio' },
+  filetypes = { 'python' },
+})
+
+-- Enable Python Language Server
+vim.lsp.enable('pyright')
+
+-- Set configuration for c# language server
+vim.lsp.config('csharp_ls', {
+  cmd = { 'csharp-language-server', '--stdio' },
+  filetypes = { 'cs' },
+})
+
+-- Enable C# Language Server
+vim.lsp.enable('csharp_ls')
